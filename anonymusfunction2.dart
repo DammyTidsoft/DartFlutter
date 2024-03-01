86f19c3fe517626f0ec87d60abcf4f4405fad4f9
DammyTidsoft <105553815+DammyTidsoft@users.noreply.github.com>
2024-03-01 09:35:37 +0100

    functions

---
+++ anonymusfunction2.dart
@@ -0,0 +1,9 @@
+void main() {
+  // list of cars
+  List cars = ["BMW", "BENZ", "AUDI", "TOYOTA"];
+
+  // iteration with anonymous function as a parameter
+  cars.forEach((car) {
+    print(car); // printing an item
+  });
+}
